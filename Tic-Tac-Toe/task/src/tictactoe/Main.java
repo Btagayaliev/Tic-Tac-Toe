d064d802eee934fd407b322a27e072d755cbdfe7
Bakhtovar <btagayaliev@gmail.com>
2020-12-22 22:03:19 +0100

    Printting the user input as the grid

--- Tic-Tac-Toe/task/src/tictactoe/Main.java
+++ Tic-Tac-Toe/task/src/tictactoe/Main.java
@@ -1,14 +1,35 @@
 package tictactoe;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         // write your code here
-        for (int i = 0; i < 3; i++) {
-            System.out.print("O");
-            for (int j = 0; j < 2; j ++) {
-                System.out.print("X");
-            }
-            System.out.println();
+        System.out.println("Enter cells:");
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.next();
+        printDashes();
+        printBody(input);
+        printDashes();
+    }
+
+    public static void printDashes() {
+        for (int i = 0; i < 9; i++) {
+            System.out.print("-");
         }
+    }
+
+    public static void printBody(String string) {
+            System.out.print(
+                    "\n| " + string.charAt(0) + " " +
+                    string.charAt(1) + " " +
+                    string.charAt(2) + " |\n" +
+                    "| " + string.charAt(3) + " " +
+                    string.charAt(4) + " " +
+                    string.charAt(5) + " |\n" +
+                    "| " + string.charAt(6) + " " +
+                    string.charAt(7) + " " +
+                    string.charAt(8) + " |\n"
+                    );
     }
 }
